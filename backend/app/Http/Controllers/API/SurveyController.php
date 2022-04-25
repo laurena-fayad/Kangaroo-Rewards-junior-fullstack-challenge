de100dadba4629fe771d357ce70c8c4c2d742648
Laurena Fayad <laurenafayad@gmail.com>
2022-04-25 13:17:22 +0300

    Backend: moved data folder to laravel's storage dir and added code to displaySurvey function in SurveyController to show 1 file

--- backend/app/Http/Controllers/API/SurveyController.php
+++ backend/app/Http/Controllers/API/SurveyController.php
@@ -7,5 +7,14 @@ use Illuminate\Http\Request;
 
 class SurveyController extends Controller
 {
-    //
+    public function displaySurveys(){
+
+        $surveys = json_decode(file_get_contents(storage_path() . "/data/1.json"), true);
+
+        return response()->json([
+            $surveys['survey']
+        ]);
+    }
 }
+
+
