19a7cd2f8c02e665c9777102aa82ed8baafc3c75
Laurena Fayad <laurenafayad@gmail.com>
2022-04-25 12:12:08 +0300

    Backend: added a get route to display surveys

--- backend/app/Http/Controllers/API/SurveyController.php
+++ backend/app/Http/Controllers/API/SurveyController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class SurveryController extends Controller
+class SurveyController extends Controller
 {
     //
 }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\API\SurveyController;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +16,8 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::get('surveys', [SurveyController::class, 'displaySurveys']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
