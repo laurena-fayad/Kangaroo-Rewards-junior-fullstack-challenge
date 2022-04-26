c0ec542eeab30f2af7bbb68068890b771512c697
Laurena Fayad <laurenafayad@gmail.com>
2022-04-26 16:23:03 +0300

    Backend: added a file count in data folder for scalability

--- backend/app/Http/Controllers/API/SurveyController.php
+++ backend/app/Http/Controllers/API/SurveyController.php
@@ -4,13 +4,20 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use File;
 
 class SurveyController extends Controller
 {
     public function displaySurveys(){
-
+        $all_files = File::files(storage_path('data'));
+        $filecount = 0;
+        
+        if ($all_files !== false) {
+            $filecount = count($all_files);
+        }
+        
         $surveys = [];
-        for ($i = 1; $i<16; $i++){
+        for ($i = 1; $i<=$filecount; $i++){
             $survey = json_decode(file_get_contents(storage_path() . "/data/".$i.".json"), true);
             if(!in_array($survey['survey'], $surveys)){
                 array_push($surveys, $survey['survey']);
