4e1a6e0220a08f0eb6a1851801927a192231b6ef
Laurena Fayad <laurenafayad@gmail.com>
2022-04-26 16:08:07 +0300

    Frontend: displayed survey data in a table inside the modal

--- frontend/app.js
+++ frontend/app.js
@@ -4,16 +4,13 @@ const getSurveys = async () => {
     await axios.get('http://localhost:8000/api/surveys')
     .then((response) => {
         let survey_data = response.data
-        console.log(survey_data)
         for (let i = 0; i<survey_data.length; i++){
             let survey_name = `${survey_data[i].name}`
             let survey_code = `${survey_data[i].code}`
-            let survey_html = survey_name + " " +survey_code + "</br>"
+            let survey_html = "<tr><td>"+ survey_name + "</td><td>" + survey_code + "</td></tr>"
             rows.innerHTML += survey_html
         }
     })
 }
 
 getSurveys();
-
-// button.addEventListener('click', getSurveys);
